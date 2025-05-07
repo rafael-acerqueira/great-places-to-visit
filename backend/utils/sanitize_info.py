77304c99814aa3cbfb54a626683614bf3f811439
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-05-07 17:35:22 -0300

    Add conditinal to check if website and phone are present

--- backend/utils/sanitize_info.py
+++ backend/utils/sanitize_info.py
@@ -6,16 +6,22 @@ def get_content_ids(data):
 
 def get_relevant_info(data):
     sections = data["data"]["AppPresentation_queryAppDetailV2"][0]["sections"]
+    phone = ''
+    website = ''
 
-    decoded_phone = urllib.parse.unquote(sections[1]["contactLinks"][1]["link"]["externalUrl"])
-    phone = decoded_phone.replace('tel:+55','')
+    if sections[1].get("contactLinks") and sections[1].get("contactLinks")[0].get("link").get("externalUrl"):
+        website = sections[1]["contactLinks"][0]["link"]["externalUrl"]
+
+    if sections[1].get("contactLinks") and len(sections[1].get("contactLinks")) > 1 and sections[1].get("contactLinks")[1].get("link").get("externalUrl"):
+        decoded_phone = urllib.parse.unquote(sections[1]["contactLinks"][1]["link"]["externalUrl"])
+        phone = decoded_phone.replace('tel:+55', '')
 
     return {
         "hero": sections[0]["heroContent"][2]["data"]["sizes"][5]["url"],
         "name": sections[1]["name"],
         "numberReviews": sections[1]["numberReviews"],
         "rating": sections[1]["rating"],
-        "website": sections[1]["contactLinks"][0]["link"]["externalUrl"],
+        "website": website,
         "phone": phone,
         "address": sections[4]["address"]["address"],
         "latitude": sections[4]["address"]["geoPoint"]["latitude"],

59e79c023907a24856a55d5dc8034841b9fb117e
Rafael Aquino <rafael.acerqueirah@gmail.com>
2025-05-08 13:02:33 -0300

    Add cors origins to .env

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,17 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import restaurants, hotels
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
+
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
